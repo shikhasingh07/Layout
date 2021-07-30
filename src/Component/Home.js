8329657de538a145f83f9f44fde8d0669a5a4e12
shikhasingh07 <shikha.thakur2295@gmail.com>
2021-07-30 17:52:00 +0530

    update

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 import SideBar from "./SideBar";
 import man from "./image/man.png";
 import "../App.css";
